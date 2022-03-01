4966dc630e0760067af9a69df797614c0b455607
Yael Krig <yaelkrig@gmail.com>
2022-03-01 21:48:57 +0200

    commit

--- src/apis/axios_api.js
+++ src/apis/axios_api.js
@@ -4,9 +4,8 @@ import axios from "axios";
 // the server port
 // grab tokrn from localstorage, same name between all frontend == accToken
 const storage = localStorage.LSToken;
-console.log("api function")
 const TOKEN = `bearer ${storage}`;
-console.log(TOKEN)
+
 
 const api = axios.create({
     baseURL: "https://myplaylist-app.herokuapp.com",
