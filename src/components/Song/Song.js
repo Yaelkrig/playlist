61fc33b556d12d42c0b7f67b0a8f84315d6cb529
Yael Krig <yaelkrig@gmail.com>
2022-03-27 14:53:18 +0300

    makeover

--- src/components/Song/Song.js
+++ src/components/Song/Song.js
@@ -8,14 +8,13 @@ import removeContext from '../../Contexts/removeContext';
 import playlistIndexContext from '../../Contexts/playlistIndexContext';
 import PlaylistsContext from '../../Contexts/PlaylistsContext';
 
-const Song = ({ song, playlistId }) => {
+const Song = ({ song, playlistId, isMy }) => {
     const { playlists } = useContext(PlaylistsContext)
     const { setPlaylists } = useContext(removeContext);
-    const { setPlaylistIndex } = useContext(playlistIndexContext);
     const { setSongPlayer } = useContext(songIndexContext);
-    const { title, url, imgUrl, _id } = { ...song }
+
+    const song_img = song.imgUrl;
     const removeSong = (playlist, song) => {
-        // setPlaylists(prePlaylists => prePlaylists.map(list => { return console.log(list); }))
         const data = JSON.stringify({
             playlistId: playlist,
             songId: song
@@ -24,38 +23,20 @@ const Song = ({ song, playlistId }) => {
             api.put('/playlists/deleteSong', data)
                 .then(res => {
                     setPlaylists(res.data);
-                    setPlaylistIndex(playlists.length - 1);
                 });
         } catch (e) {
             console.log(e);
         }
     }
     return (
-        <ListItem className='Song'
-            secondaryAction={
-                <Tooltip title="delete song" placement="right-start">
-                    <IconButton edge="end" aria-label="delete"
-                        onClick={() => {
-                            removeSong(playlistId, _id);
-                        }}>
-                        <DeleteIcon className='delete_bt' />
-                    </IconButton>
-                </Tooltip>
-            }>
-            <span className='play_song'
-                onClick={() => {
-                    setSongPlayer(url);
-                }}>
-                <ListItemAvatar >
-                    <Avatar
-                        sx={{ width: 70, height: 70 }}
-                    >
-                        <img className='song_img' alt={title} src={imgUrl} />
-                    </Avatar>
-                </ListItemAvatar>
-                <ListItemText className="description" />{title}
-            </span>
-        </ListItem >
+        <div key={song._id} className='song'
+            onClick={() => { setSongPlayer(song.url) }}>
+
+            <img className='song_img' src={song_img} alt='song_img' />
+            <p className='song_title'>{song.title}</p>
+            {isMy && <DeleteIcon className='delete_song'
+                onClick={() => removeSong} />}
+        </div>
     )
 }
 export default Song;
